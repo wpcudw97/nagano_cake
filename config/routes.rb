00119426da3e99d13f7ee9d925051c6419997e5b
wpcudw97 <114921824+wpcudw97@users.noreply.github.com>
2022-12-06 15:27:24 +0000

    管理者ログイン画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :customers
   devise_for :admins
+
+  # root to: "public/homes#top"
+
+  # 会員側のルーティング設定
+  resources :items
+
+  # 管理者側のルーティング設定
+  namespace :admin do
+    resources :items
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
