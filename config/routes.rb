b079758ac89afce310d21bbc7c764605f607d658
wpcudw97 <114921824+wpcudw97@users.noreply.github.com>
2022-12-08 15:31:01 +0000

    root toの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   devise_for :customers
   devise_for :admins
 
-  # root to: "public/homes#top"
-
   # 会員側のルーティング設定
-  resources :items
+    root to: "public/homes#top"
+    resources :items
 
   # 管理者側のルーティング設定
   namespace :admin do
+    root to: "homes#top"
+    get "/home/about" => "admin/homes#about", as: "about"
     resources :items
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
