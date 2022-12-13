c9a78ac225916864916f47f2d61e68807a68b9d4
wpcudw97 <114921824+wpcudw97@users.noreply.github.com>
2022-12-13 10:59:21 +0000

    customerのcontroller作成中

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     get "home/about" => "homes#about", as: "about"
     resources :items, only:[:index, :new, :create, :show, :edit, :update]
     resources :genres, only:[:index, :create, :edit, :update]
+    resources :customers, only:[:index, :show, :edit, :update]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
