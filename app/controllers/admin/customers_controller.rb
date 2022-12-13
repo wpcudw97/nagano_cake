282dda73dc12eb2adf8f1c99964db6676b4b99a2
wpcudw97 <114921824+wpcudw97@users.noreply.github.com>
2022-12-13 14:35:00 +0000

    新規会員登録を作成後、admin/customer確認

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CustomersController < ApplicationController
   def index
-    @customers = Customer.all
+    @customers = Customer.page(params[:page])
   end
 
   def show
@@ -16,10 +16,10 @@ class Admin::CustomersController < ApplicationController
     @customer.update(customer_params)
     redirect_to  admin_customers_path
   end
-  
+
    private
 
   def customer_params
-    params.require(:customer).permit(:last_name, :)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :postal_code, :address, :telephone_number, :is_deleted)
   end
 end
