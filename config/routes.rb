d2bdaff2f37500774940fc68910e6ddfbd19b571
wpcudw97 <114921824+wpcudw97@users.noreply.github.com>
2022-12-14 15:37:08 +0000

    新規会員登録型完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
+  devise_for :customers,controllers: {
+    sessions: "public/sessions",
+    passwords: "public/passwords",
+    registrations: "public/registrations"
+  }
+  devise_for :admins,controllers: {
+    sessions: "admin/sessions",
+    passwords: "admin/passwords",
+    registrations: "admin/registrations"
+  }
 
   # 会員側のルーティング設定
   root to: "public/homes#top"
