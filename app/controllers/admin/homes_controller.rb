26043ffaa841de33120b2d1633c4717b011e2e6d
wpcudw97 <114921824+wpcudw97@users.noreply.github.com>
2022-12-15 04:07:33 +0000

    public/homes/top作成途中

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,5 @@
 class Admin::HomesController < ApplicationController
   def top
+    @tops = Top.all
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -95,6 +95,11 @@ ActiveRecord::Schema.define(version: 2022_12_08_155455) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "homes", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "items", force: :cascade do |t|
     t.integer "genre_id", null: false
     t.string "name", null: false
