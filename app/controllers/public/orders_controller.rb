65adc6c5a8533a22ec1fe5082505493753cf23d0
wpcudw97 <114921824+wpcudw97@users.noreply.github.com>
2022-12-30 04:28:09 +0000

    配送先住所のcontroller設定完了

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,7 +4,22 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
-
+    @order = Order.new(order_params)
+    if params[:order][:select_address] == "0"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name + current_customer.last_name
+    elsif params[:order][:select_address] == "1"
+       @address = Address.find(params[:order][:address_id])
+       @order.postal_code = @address.postal_code
+       @order.address = @address.address
+       @order.name = @address.name
+    elsif params[:order][:select_address] == "2"
+      @order.customer_id = current_customer.id
+    end
+      @cart_items = current_customer.cart_items
+      @order_new = Order.new
+      render :confirm
   end
 
   def complete
