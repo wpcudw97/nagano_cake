94289f6d32a8cb8886f21dea9bb6c1af0fbb1bb0
wpcudw97 <114921824+wpcudw97@users.noreply.github.com>
2023-01-09 07:39:58 +0000

    ステータスの自動更新機能実装完了

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,9 +1,24 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
+    @order = Order.find(params[:id])
     @order_detail = OrderDetail.find(params[:id])
-    @order_detail.update(order_details_params)
+    @order_details = @order.order_details.all
+    is_updated = true
+    if @order_detail.update(order_details_params)
+      @order.update(status: 2) if @order_detail.making_status == "production"
+      @order_details.each do |order_detail|
+        if order_detail.making_status != "completion_of_production"
+          is_updated = false
+        end
+      end
+      @order.update(status: 3) if is_updated
+    end
     redirect_to admin_order_path(@order_detail.order)
   end
+  #   @order_detail = OrderDetail.find(params[:id])
+  #   @order_detail.update(order_details_params)
+  #   redirect_to admin_order_path(@order_detail.order)
+  # end
 
   private
 
