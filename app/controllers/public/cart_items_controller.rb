8c59045776a51690ceb0087ceb085917c3a6372e
wpcudw97 <114921824+wpcudw97@users.noreply.github.com>
2023-01-13 22:59:17 +0000

    cart_items_controllerのcreate修正完了

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -16,6 +16,8 @@ class Public::CartItemsController < ApplicationController
         @cart_item.delete
       end
     end
+    @cart_item.save
+    redirect_to cart_items_path
   end
 
   def update
